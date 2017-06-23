0a1129190b40858e7d1b6136031638b0832ada11
angus.chen <angus.chen@anguschende-MacBook-Pro.local>
2017-06-23 10:17:44 +0800

    core version >= 6.0.0

---
+++ test/api/controllers/lottery.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+describe('Lucky Lottery', function(){
+    describe('#check_number()', function(){
+        it('check input number available', function(){
+            assert.exist(target, 'must input your numbers');
+            assert.isString(target, 'number must be string type.');
+        })
+        /*it('check input number format', function(){
+            assert.
+        })*/
+        it('')
+    })
+})
